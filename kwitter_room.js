cc024167661b3c3589be9ccc49182da9b5730c90
monish2011 <142167411+monish2011@users.noreply.github.com>
2024-02-14 19:33:22 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -11,20 +11,23 @@ const firebaseConfig = {
       appId: "1:635590586872:web:973f4b465bf92258f0e60d"
     };
     user_name = localStorage.getItem("user_name");
-    document.getElementById("user_name").innerHTML = "Welcome" + user_name + "!";
+    document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
     
     firebase.initializeApp(firebaseConfig);
     function addRoom(){
+      window.location.replace("kwitter_page.html");    
+
       room_name = document.getElementById("room_name").value;
       firebase.database().ref("/").child(room_name).update({
             purpose: "adding room name"   });
+      window.location.replace("kwitter_page.html");
     }
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
       console.log("Room Names - " + Room_names);
       row = "<div class='room_name' id="+Room_names+" onclick = 'redirectToRoomName(this.id)'># " + Room_names + "</div> <hr>";
-      document.getElementById("output").innerHTML = row;
+      document.getElementById("output").innerHTML += row;
 
 
       //End code
